e7dc00aa467f43bffff238ce8e676f6148d4626c
chenyu <chenyu1520@gmail.com>
2016-12-18 01:06:28 +0800

    1.添加09_Fibonacci 斐波那契数列算法 2.添加10_NumberOf1 一个数字用二进制表示后里面包含1的个数

---
+++ 09_Fibonacci/09_Fibonacci/main.cpp
@@ -0,0 +1,73 @@
+//
+//  main.cpp
+//  09_Fibonacci
+//
+//  Created by chenyu on 2016/12/17.
+//  Copyright © 2016年 ChenYu. All rights reserved.
+//
+
+#include <iostream>
+
+// ====================方法1：递归====================
+long long Fibonacci_Solution1(unsigned int n)
+{
+    if(n <= 0)
+        return 0;
+    
+    if(n == 1)
+        return 1;
+    
+    return Fibonacci_Solution1(n - 1) + Fibonacci_Solution1(n - 2);
+}
+
+// ====================方法2：循环====================
+long long Fibonacci_Solution2(unsigned n)
+{
+    int result[2] = {0, 1};
+    if(n < 2)
+        return result[n];
+    
+    long long  fibNMinusOne = 1;
+    long long  fibNMinusTwo = 0;
+    long long  fibN = 0;
+    for(unsigned int i = 2; i <= n; ++ i)
+    {
+        fibN = fibNMinusOne + fibNMinusTwo;
+        
+        fibNMinusTwo = fibNMinusOne;
+        fibNMinusOne = fibN;
+    }
+    
+    return fibN;
+}
+
+// ====================测试代码====================
+void Test(int n, int expected)
+{
+    if(Fibonacci_Solution1(n) == expected)
+        printf("Test for %d in solution1 passed.\n", n);
+    else
+        printf("Test for %d in solution1 failed.\n", n);
+    
+    if(Fibonacci_Solution2(n) == expected)
+        printf("Test for %d in solution2 passed.\n", n);
+    else
+        printf("Test for %d in solution2 failed.\n", n);
+}
+
+int main(int argc, const char * argv[]) {
+    Test(0, 0);
+    Test(1, 1);
+    Test(2, 1);
+    Test(3, 2);
+    Test(4, 3);
+    Test(5, 5);
+    Test(6, 8);
+    Test(7, 13);
+    Test(8, 21);
+    Test(9, 34);
+    Test(10, 55);
+    
+    Test(40, 102334155);
+    return 0;
+}
